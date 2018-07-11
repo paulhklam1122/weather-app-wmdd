b80dd46e8aa6e53bf40e0970aa4459b593dbd7de
Paul Lam <paulhklam1122@gmail.com>
2018-07-11 01:33:19 -0700

    finish grid structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
 
 class App extends Component {
   render () {
     return (
-      <h1>Hello World</h1>
+      <div className='wrapper'>
+        <div className='main'>
+          <Grid container spacing={0}>
+            <Grid item xs={5}>
+            </Grid>
+            <Grid item xs={7} className='information-container'>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
     )
   }
 }
